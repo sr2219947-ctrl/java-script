e355f549323ba42cff4488448fdea4f60568785f
Sakshi <sr2219947@gmail.com>
2026-01-17 21:08:21 +0530

    java script variables data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId= 1454332;
+let accountEmail="ser@gmail.com";
+var accountPassword="12345";
+ let accountCity="Jaipur";
+accountEmail="kuk@gmail.com";
+accountPassword="3454642";
+accountCity="Bengaluru";
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+// let isloggedIn=" " this string has a sopace , so it is not empty
+// let booleanIsloggedIn= Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+let isloggedIn=null;
+// console.log(Boolean(isloggedIn)); //false   case: isloggedIn=""
+// console.log(Boolean(isloggedIn));
+const score=100;
+const scorevalue=100.3;
+let userEmail;
+const id=Symbol('123')
+const anotherId= Symbol('123')
+const bigNumber =32435454345352n
+// console.log(id==anotherId);
+const hero=["hanuman", "superman", "thor"] ;//array 
+let myobj={
+    name:"sakshi",
+    age:22
+};
+
+//     const myfunction =function()
+// {
+//     console.log("hello world");
+// };
+// myfunction();
+console.log(typeof bigNumber);
