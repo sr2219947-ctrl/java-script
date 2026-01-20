f6c10747b62f2f7d6acd8ab4e1409611c3037345
Sakshi <sr2219947@gmail.com>
2026-01-20 22:01:31 +0530

    string basics

---
+++ 01_basics/04.string.js
@@ -0,0 +1,14 @@
+// string practice
+let rawName ="   SakshiRana   ";
+console.log("---TRIM()--");
+console.log("Before trim:", `"${rawName}"`);
+console.log("After trim :",`"${rawName.trim()}"`);
+console.log("\n");
+//  replace  and trim
+let url ="https://www.canva.com/photo-editor/";
+console.log("--Replace()--");
+console.log("Original URL:", url);
+console.log("replace('canva','canvas stdio'):",url.replace("canva","canvas stdio"));
+console.log("replaceAll('canva','canvas stdio'):",url.replaceAll("canva","canvas stdio"));
+console.log("includes'canva'):",url.includes("canva"));
+console.log("\n");
