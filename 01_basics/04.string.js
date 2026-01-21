04a7f4fca739adcd95309bb7c072699e4379c9f6
Sakshi <sr2219947@gmail.com>
2026-01-21 22:50:37 +0530

    string practice

--- 01_basics/04.string.js
+++ 01_basics/04.string.js
@@ -12,3 +12,36 @@ console.log("replace('canva','canvas stdio'):",url.replace("canva","canvas stdio
 console.log("replaceAll('canva','canvas stdio'):",url.replaceAll("canva","canvas stdio"));
 console.log("includes'canva'):",url.includes("canva"));
 console.log("\n");
+//slice()-> 
+let gamename="chess-sc-com";
+console.log("--slice()--");
+console.log("Original string:", gamename);
+console.log("slice(0,4):",gamename.slice(0,6)); 
+console.log("slice(5,6) :",gamename.slice(5,6));
+console.log("slice(7,9):",gamename.slice(7,9));
+console.log("slice(6,9):",gamename.slice(6,9));
+console.log("slice(9) :", gamename.slice(9));
+console.log("slice(-3):",gamename.slice(-3));
+console.log("slice(-6,-4):",gamename.slice(-6,-4));
+console.log("\n");
+ //mixed examples
+//user input with spaces 
+let userinput ="  sakshi   ";
+let cleanname=userinput.trim();
+console.log("user input: ", `${userinput}`);
+console.log("clean name :",`${cleanname}`);
+//url formatting
+let blogurl ="https://www.canva.com/photo-editor/";
+let cleanurl =blogurl.replaceAll("canva","canvas stdio");
+ 
+console.log("original blog url:", blogurl);
+console.log("seo friendly url:",cleanurl);
+ 
+// extract domain part
+let domain= blogurl.slice(8,21);
+console.log("extracted domain:", domain);
+
+
+ 
+
+   
