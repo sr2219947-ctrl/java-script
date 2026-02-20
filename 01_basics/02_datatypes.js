496d68778188cdc4c2923ab47ac3f8cbec444c27
Sakshi <sr2219947@gmail.com>
2026-02-21 05:18:49 +0530

    pratice string

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,4 +1,4 @@
-// let isloggedIn=" " this string has a sopace , so it is not empty
+// let isloggedIn=" " this string has a space , so it is not empty
 // let booleanIsloggedIn= Boolean(isloggedIn)
 // console.log(booleanIsloggedIn);
 let isloggedIn=null;
@@ -23,3 +23,22 @@ let myobj={
 // };
 // myfunction();
 console.log(typeof bigNumber);
+ //primitive data types 
+ const num=10;
+ console.log(num);
+//  num=20;// error because num is a constant variable and cannot be reassigned
+//  console.log(num);
+ //non primitive data types
+ const obj={
+    id:10,
+    balance:1000
+ }
+ obj.id=20;// we can change the properties of an object even if it is declared as const
+console.log(obj);
+let obj2={
+    id:20,
+    money:30
+};
+// obj=obj2;// we can reassign obj to obj2 because obj is declared as let
+// console.log(obj); //error because obj is declared as const and cannot be reassigned and 
+// **imp: adress of obj is changed to obj2 it **
--- 01_basics/04.string.js
+++ 01_basics/04.string.js
@@ -1,3 +1,38 @@
+ let str1="hello i am learning js\n";
+ let str2='js is a programming language\n';
+ let str3=`aur btao kaise ho?`;
+ console.log(str1,str2,str3);
+  
+ let s1="hello";
+ let s2="world";
+ console.log(s1+" "+s2);
+ console.log(str3.length);
+ console.log(str3.toUpperCase());
+ console.log(str3.toLowerCase());
+ 
+ console.log('"Hello ji');
+ console.log('\'Hello ji\'');
+ let comment ="this is comment \\n this is not a new line ";
+ console.log(comment);
+
+// let price=80;
+// console.log(`price of fresh tomato is ${price}, get it fast`);
+// console.log(` the sum of 2 and 3 is ${2+3}`);
+
+// let  mountains=" himalayas are beautiful";
+// console.log(mountains.indexOf("are"));
+// console.log(mountains.lastIndexOf("are"));
+// console.log(mountains.includes("beautiful"));
+//  //0123456
+
+let newstring= "helodon" ; // -7 -6 -5 -4 -3 -2 -1  & -0 not exist in string
+console.log(newstring.slice(0,3)); //includes 0 but excludes 3
+console.log(newstring.slice(-6,5)); //includes -6 but excludes 5
+console.log(newstring.slice(-2,4)); //includes -2 but excludes 4 so it returns empty string why? beacuse they are in same order .
+console.log(newstring.slice(2,-1));//includes 2 but excludes -4" 
+console.log(newstring.substring(0,3)); //cannot use negative index in substring 
+
+
 // string practice
 let rawName ="   SakshiRana   ";
 console.log("---TRIM()--");
